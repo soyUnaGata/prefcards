4ddc15dbeb661ce13a232902cedec804d7fe696a
soyUnaGata <fallen.snitch@gmail.com>
2025-01-01 20:52:00 +0200

    implement for test to get in json format response

--- app/controllers/PrefCardController.scala
+++ app/controllers/PrefCardController.scala
@@ -7,9 +7,15 @@ import play.api.mvc.{BaseController, ControllerComponents}
 import javax.inject.Inject
 import scala.collection.mutable
 
+import play.api.libs.json._
+
+
 class PrefCardController @Inject()(val controllerComponents: ControllerComponents)
   extends BaseController{
   private val prefCard = new mutable.ListBuffer[PrefCardItem]()
   prefCard += PrefCardItem( 1, "Dr. Ivanov", "Orthopedics", "Scalpel, Clamp, Needle Holder", "90 minutes")
   prefCard += PrefCardItem( 2, "Dr. Smirnov", "Cardiac Surgery", "Scalpel, Suture Material, ECG Machine", "120 minutes")
+
+  implicit val prefCardJson: OFormat[PrefCardItem] = Json.format[PrefCardItem]
+
 }
