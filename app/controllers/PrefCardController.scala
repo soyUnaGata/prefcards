a922d9408d4c7b06b76633535da100a84b11eba4
soyUnaGata <fallen.snitch@gmail.com>
2025-01-02 18:38:37 +0200

    implement method to edit card

--- app/controllers/PrefCardController.scala
+++ app/controllers/PrefCardController.scala
@@ -3,6 +3,7 @@ package controllers
 import com.fasterxml.jackson.module.scala.deser.overrides.MutableList
 import models.{NewPrefCardItem, PrefCardItem}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import slick.jdbc.H2Profile.api._
 
 import javax.inject.Inject
 import scala.collection.mutable
@@ -53,6 +54,39 @@ class PrefCardController @Inject()(val controllerComponents: ControllerComponent
     }
   }
 
+  def editCard(itemId: Long) = Action { implicit request =>
+    val content = request.body
+    val jsonObject = content.asJson
+    val updatedCard: Option[NewPrefCardItem] =
+      jsonObject.flatMap(Json.fromJson[NewPrefCardItem](_).asOpt)
+
+    updatedCard match {
+      case Some(newItem) =>
+        val index = prefCard.indexWhere(_.id == itemId)
+
+        if (index != -1) {
+          val updated = PrefCardItem(
+            id = itemId,
+            name = newItem.name,
+            operation = newItem.operation,
+            tools = newItem.tools,
+            duration = newItem.duration
+          )
+
+          prefCard.update(index, updated)
+
+          saveCards(prefCard)
+
+          Ok(Json.toJson(updated))
+        } else {
+          NotFound(Json.obj("error" -> s"Item with ID $itemId not found"))
+        }
+
+      case None => BadRequest(Json.obj("error" -> "Invalid data"))
+    }
+  }
+
+
   def deleteCard(itemId: Long) = Action {
     val foundItem = prefCard.find(_.id == itemId)
     foundItem match {
@@ -61,5 +95,10 @@ class PrefCardController @Inject()(val controllerComponents: ControllerComponent
         NoContent
       case None => NotFound(Json.obj("error" -> s"Item with ID $itemId not found"))
     }
+  }
+
+  def saveCards(card: PrefCardItem): DBIO[Int] = {
+    val query = TableQuery[PrefCardItems].filter(_.id === card.id)
+    query.update(card)
   }
 }
