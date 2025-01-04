9f72df0bca85142360133d95aadb98e92888e928
soyUnaGata <fallen.snitch@gmail.com>
2025-01-04 16:29:52 +0200

    implement methods for CRUD operations

--- app/models/PrefCardItem.scala
+++ app/models/PrefCardItem.scala
@@ -3,6 +3,8 @@ package models
 import play.api.db.slick.HasDatabaseConfigProvider
 import slick.lifted.ProvenShape
 
+import scala.concurrent.Future
+
 case class PrefCardItem(id: Long, name:String,
                      operation: String, tools: String,
                      duration: String)
@@ -19,6 +21,26 @@ trait PrefCardTable extends HasDatabaseConfigProvider[slick.jdbc.JdbcProfile] {
     def tools: Rep[String] = column[String]("tools")
     def duration: Rep[String] = column[String]("duration")
     def * : ProvenShape[PrefCardItem] = (id, name, operation, tools, duration) <>(PrefCardItem.tupled, PrefCardItem.unapply)
+  }
+
+  def insertPrefCard(prefCard: PrefCardItem): Future[Int] = {
+    db.run(prefcardQuery += prefCard)
+  }
+
+  def getPrefCards: Future[Seq[PrefCardItem]] = {
+    db.run(prefcardQuery.result)
+  }
+
+  def getPrefCardById(id: Long): Future[Option[PrefCardItem]] = {
+    db.run(prefcardQuery.filter(_.id === id).result.headOption)
+  }
+
+  def updatePrefCard(prefCard: PrefCardItem): Future[Int] = {
+    db.run(prefcardQuery.filter(_.id === prefCard.id).update(prefCard))
+  }
+
+  def deletePrefCard(id: Long): Future[Int] = {
+    db.run(prefcardQuery.filter(_.id === id).delete)
   }
 }
 
