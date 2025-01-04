af8d0b58f49130ff7801f9b50ff6545aeb096e03
soyUnaGata <fallen.snitch@gmail.com>
2025-01-04 16:33:57 +0200

    implement methods for CRUD operations

--- app/controllers/PrefCardController.scala
+++ app/controllers/PrefCardController.scala
@@ -1,104 +1,51 @@
 package controllers
 
-import com.fasterxml.jackson.module.scala.deser.overrides.MutableList
-import models.{NewPrefCardItem, PrefCardItem}
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
-import slick.jdbc.H2Profile.api._
+import javax.inject._
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+import models.{PrefCardTable, PrefCardItem}
 
-import javax.inject.Inject
-import scala.collection.mutable
-import play.api.libs.json._
+@Singleton
+class PrefCardController @Inject()(cc: ControllerComponents, prefCardTable: PrefCardTable)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-
-class PrefCardController @Inject()(val controllerComponents: ControllerComponents)
-  extends BaseController{
-
-  private val prefCard = new mutable.ListBuffer[PrefCardItem]()
-  prefCard += PrefCardItem( 1, "Dr. Ivanov", "Orthopedics", "Scalpel, Clamp, Needle Holder", "90 minutes")
-  prefCard += PrefCardItem( 2, "Dr. Smirnov", "Cardiac Surgery", "Scalpel, Suture Material, ECG Machine", "120 minutes")
-
-  implicit val prefCardJson: OFormat[PrefCardItem] = Json.format[PrefCardItem]
-  implicit val newPrefCardJson: OFormat[NewPrefCardItem] = Json.format[NewPrefCardItem]
-
-
-  def getAll(): Action[AnyContent] = Action {
-    if (prefCard.isEmpty) {
-      NoContent
-    } else {
-      Ok(Json.toJson(prefCard))
-    }
-  }
-
-  def getById(itemId: Long) = Action {
-    val foundItem = prefCard.find(_.id == itemId)
-    foundItem match {
-      case Some(item) => Ok(Json.toJson(item))
-      case None => NotFound(Json.obj("error" -> s"Item with ID $itemId not found"))
-    }
-  }
-
-  def addCard() = Action { implicit request =>
-    val content = request.body
-    val jsonObject = content.asJson
-    val prefCardItem: Option[NewPrefCardItem] =
-      jsonObject.flatMap(
-        Json.fromJson[NewPrefCardItem](_).asOpt
-      )
-    prefCardItem match {
-      case Some (newItem) =>
-        val nextId = prefCard.map(_.id).max + 1
-        val toBeAdded = PrefCardItem(nextId, newItem.name, newItem.operation, newItem.tools, newItem.duration)
-        prefCard += toBeAdded
-        Created(Json.toJson(toBeAdded))
-      case None => BadRequest
-    }
-  }
-
-  def editCard(itemId: Long) = Action { implicit request =>
-    val content = request.body
-    val jsonObject = content.asJson
-    val updatedCard: Option[NewPrefCardItem] =
-      jsonObject.flatMap(Json.fromJson[NewPrefCardItem](_).asOpt)
-
-    updatedCard match {
-      case Some(newItem) =>
-        val index = prefCard.indexWhere(_.id == itemId)
-
-        if (index != -1) {
-          val updated = PrefCardItem(
-            id = itemId,
-            name = newItem.name,
-            operation = newItem.operation,
-            tools = newItem.tools,
-            duration = newItem.duration
-          )
-
-          prefCard.update(index, updated)
-
-          saveCards(prefCard)
-
-          Ok(Json.toJson(updated))
-        } else {
-          NotFound(Json.obj("error" -> s"Item with ID $itemId not found"))
+  def addCard = Action.async(parse.json) { request =>
+    request.body.validate[PrefCardItem].fold(
+      errors => Future.successful(BadRequest("Invalid data")),
+      prefCard => {
+        prefCardTable.insertPrefCard(prefCard).map { _ =>
+          Ok("PrefCard added")
         }
+      }
+    )
+  }
 
-      case None => BadRequest(Json.obj("error" -> "Invalid data"))
+  def getAll = Action.async {
+    prefCardTable.getPrefCards.map { prefCards =>
+      Ok(views.html.prefcards(prefCards))
     }
   }
 
-
-  def deleteCard(itemId: Long) = Action {
-    val foundItem = prefCard.find(_.id == itemId)
-    foundItem match {
-      case Some(_) =>
-        prefCard -= foundItem.get
-        NoContent
-      case None => NotFound(Json.obj("error" -> s"Item with ID $itemId not found"))
+  def getById(id: Long) = Action.async {
+    prefCardTable.getPrefCardById(id).map {
+      case Some(prefCard) => Ok(views.html.prefcard(prefCard))
+      case None => NotFound("PrefCard not found")
     }
   }
 
-  def saveCards(card: PrefCardItem): DBIO[Int] = {
-    val query = TableQuery[PrefCardItems].filter(_.id === card.id)
-    query.update(card)
+  def updateCard(id: Long) = Action.async(parse.json) { request =>
+    request.body.validate[PrefCardItem].fold(
+      errors => Future.successful(BadRequest("Invalid data")),
+      prefCard => {
+        prefCardTable.updatePrefCard(prefCard).map { _ =>
+          Ok("PrefCard updated")
+        }
+      }
+    )
+  }
+
+  def deleteCard(id: Long) = Action.async {
+    prefCardTable.deletePrefCard(id).map { _ =>
+      Ok("PrefCard deleted")
+    }
   }
 }
